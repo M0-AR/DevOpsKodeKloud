2c918fb6a092eac76a6efdc7d357322a5e6993a9
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-04-20 17:02:24 +0200

    Puppet Setup Database	kodekloud

---
+++ puppet/setup-database.pp
@@ -0,0 +1,29 @@
+class mysql_database {
+
+  # Install the mariadb-server package and start its service
+  package { 'mariadb-server':
+    ensure => installed,
+  }
+
+  service { 'mariadb':
+    ensure => running,
+    enable => true,
+    require => Package['mariadb-server'],
+  }
+
+  # Install the puppetlabs/mysql module to manage MySQL resources
+  include '::mysql::server'
+
+  # Create the database and user with the specified credentials
+  mysql::db { 'kodekloud_db3':
+    user     => 'kodekloud_cap',
+    password => 'dCV3szSGNA',
+    host     => 'localhost',
+    grant    => ['SELECT', 'UPDATE'],
+    require  => Service['mariadb'],
+  }
+}
+
+node 'stdb01.stratos.xfusioncorp.com' {
+    include mysql_database
+}
